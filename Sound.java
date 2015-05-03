65c9a88abb1fd381ddbc7c9f76d7dcb2ecc4bf1c
Mathwe <Mathwe@users.noreply.github.com>
2015-05-03 08:57:28 -0500

    Create Sound.java
    Sound class of the game

---
+++ Sound.java
@@ -0,0 +1,9 @@
+package miniTennis;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+
+public class Sound {
+	public static final AudioClip Ball = Applet.newAudioClip(Sound.class.getResource("ball.wav"));
+
+}
